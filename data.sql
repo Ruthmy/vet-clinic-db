5f651e638b83ccbe37c202ab5e9c08165850201f
Ruthmy <Ruryemo.exe@gmail.com>
2023-08-19 13:02:33 -0600

    Modified 'data.sql' - Added owners table data. - Added species table data. - Added species and owners references in the animals table.

--- data.sql
+++ data.sql
@@ -14,3 +14,33 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES ('Boarmon', '2005-06-07', 7, TRUE, 20.4, 'unknown');
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES ('Blossom', '1998-10-13', 3, TRUE, 17, 'unknown');
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species) VALUES ('Ditto', '2022-05-14', 4, TRUE, 22, 'unknown');
+
+/* From task: query multiple tables */
+
+-- Insert data into the owners table.
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+-- Insert data into the species table.
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+-- Modify animals: If the name ends in "mon" it will be Digimon
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+-- Modify animals: All other animals are Pokemon
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+-- Sam Smith owns Agumon.
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+-- Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals SET owner_id = 2 WHERE(name = 'Pikachu' OR name = 'Gabumon');
+-- Bob owns Devimon and Plantmon.
+UPDATE animals SET owner_id = 3 WHERE(name = 'Devimon' OR name = 'Plantmon');
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE animals SET owner_id = 4 WHERE(name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom');
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE animals SET owner_id = 5 WHERE(name = 'Angemon' OR name = 'Boarmon');
