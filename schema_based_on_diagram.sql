59a83fd1000a0357ab8a8a1fd70dffa9ae199e4b
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-23 13:16:17 -0400

    Create the medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS patients (
     id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255),
+    name VARCHAR NOT NULL,
     date_of_birth DATE,
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP NOT NULL,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR NOT NULL,
+    PRIMARY KEY (id)
+);
